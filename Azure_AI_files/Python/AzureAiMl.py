3d3ca3291ec4c3064b6679a9e48049c5081fd10c
Michal Feldman <michal@apiiro.com>
2023-10-22 21:37:29 +0300

    changed example

--- Azure_AI_files/Python/AzureAiMl.py
+++ Azure_AI_files/Python/AzureAiMl.py
@@ -1,25 +1,19 @@
-import os
 from azure.ai.ml import MLClient
 from azure.identity import DefaultAzureCredential
 
-def create_ml_client(subscription_id, resource_group, workspace):
-    """
-    Create an Azure Machine Learning client using the provided credentials.
+class AzureMLClient:
+    def __init__(self, subscription_id, resource_group, workspace):
+        self.subscription_id = subscription_id
+        self.resource_group = resource_group
+        self.workspace = workspace
+        self.ml_client = self._create_ml_client()
 
-    Args:
-        subscription_id (str): Azure subscription ID.
-        resource_group (str): Azure resource group name.
-        workspace (str): Azure Machine Learning workspace name.
-
-    Returns:
-        MLClient: Azure Machine Learning client instance.
-    """
-    try:
-        # Create MLClient with Azure Identity DefaultAzureCredential
-        ml_client = MLClient()
-        return ml_client
-    except Exception as e:
-        raise Exception("Failed to create MLClient: " + str(e))
+    def _create_ml_client(self):
+        try:
+            ml_client = MLClient(DefaultAzureCredential(), self.subscription_id, self.resource_group, self.workspace)
+            return ml_client
+        except Exception as e:
+            raise Exception("Failed to create MLClient: " + str(e))
 
 if __name__ == "__main__":
     subscription_id = "your_subscription_id"
@@ -27,7 +21,8 @@ if __name__ == "__main__":
     workspace = "your_workspace_name"
 
     try:
-        ml_client = create_ml_client(subscription_id, resource_group, workspace)
+        azure_ml = AzureMLClient(subscription_id, resource_group, workspace)
+        ml_client = azure_ml.ml_client
         # Use ml_client for further operations
         print("Azure Machine Learning client created successfully.")
     except Exception as e:
