8480780c126a56696367ca08eb46ecd2ca57f604
Michal Feldman <michal@apiiro.com>
2023-10-23 10:13:52 +0300

    changed example

--- Azure_AI_files/Dotnet/azure_openAI_example.cs
+++ Azure_AI_files/Dotnet/azure_openAI_example.cs
@@ -1,24 +1,46 @@
 using Azure.AI.OpenAI;
 
-// Initialize the Azure OpenAI client
-OpenAIClient client = new OpenAIClient();
-
-// Build completion options object
-ChatCompletionsOptions chatCompletionsOptions = new ChatCompletionsOptions()
+public class OpenAIClientService
 {
-    Messages =
+    private OpenAIClient _client;
+
+    public OpenAIClientService()
     {
-       new ChatMessage(ChatRole.System, "You are a helpful assistant. Summarize the following text in 60 words or less."),
-       new ChatMessage(ChatRole.User, text),
-    },
-    MaxTokens = 120,
-    Temperature = 0.7f,
-};
+        _client = new OpenAIClient();
+    }
 
-// Send request to Azure OpenAI model
-ChatCompletions response = client.GetChatCompletions(
-    deploymentOrModelName: oaiModelName, 
-    chatCompletionsOptions);
-string completion = response.Choices[0].Message.Content;
+    public string GetChatCompletion(string text, string oaiModelName)
+    {
+        ChatCompletionsOptions chatCompletionsOptions = new ChatCompletionsOptions()
+        {
+            Messages =
+            {
+                new ChatMessage(ChatRole.System, "You are a helpful assistant. Summarize the following text in 60 words or less."),
+                new ChatMessage(ChatRole.User, text),
+            },
+            MaxTokens = 120,
+            Temperature = 0.7f,
+        };
 
-Console.WriteLine("Summary: " + completion + "\n");
+        ChatCompletions response = _client.GetChatCompletions(
+            deploymentOrModelName: oaiModelName,
+            chatCompletionsOptions);
+
+        string completion = response.Choices[0].Message.Content;
+        return completion;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string oaiModelName = "your_model_name"; // Replace with your actual model name
+        string text = "Your input text goes here.";
+
+        var openAIClientService = new OpenAIClientService();
+        string completion = openAIClientService.GetChatCompletion(text, oaiModelName);
+
+        Console.WriteLine("Summary: " + completion);
+    }
+}
