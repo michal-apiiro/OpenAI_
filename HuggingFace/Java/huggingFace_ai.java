b9c1c1d7e91cc191e9592c6a3d8788e2b45bd09a
Michal Feldman <michal@apiiro.com>
2023-10-25 09:58:29 +0300

    added example of huggingface java

--- HuggingFace/Java/huggingFace_ai.java
+++ HuggingFace/Java/huggingFace_ai.java
@@ -13,7 +13,30 @@ import ai.djl.huggingface.tokenizers.HuggingFaceTokenizer;
 
 
 
- 
+ public void testTokenizerDecoding() throws IOException {
+        long[][] testIds = {
+            {101, 8667, 117, 194, 112, 1155, 106, 1731, 1132, 1128, 100, 136, 102},
+            {101, 3570, 1110, 170, 21162, 1285, 119, 2750, 4250, 146, 112, 173, 1474, 102}
+        };
+        String[] expectedDecodedNoSpecialTokens = {
+            "Hello, y ' all! How are you?", "Today is a sunny day. Good weather I ' d say"
+        };
+        String[] expectedDecodedWithSpecialTokens = {
+            "[CLS] Hello, y ' all! How are you [UNK]? [SEP]",
+            "[CLS] Today is a sunny day. Good weather I ' d say [SEP]"
+        };
+        try (HuggingFaceTokenizer tokenizer = HuggingFaceTokenizer.newInstance("bert-base-cased")) {
+            for (int i = 0; i < testIds.length; ++i) {
+                String ret = tokenizer.decode(testIds[i]);
+                Assert.assertEquals(ret, expectedDecodedWithSpecialTokens[i]);
+                ret = tokenizer.decode(testIds[i], true);
+                Assert.assertEquals(ret, expectedDecodedNoSpecialTokens[i]);
+            }
+        }
+
+
+    }
+
 public final class QuestionAnswering {
 
     private QuestionAnswering() {}
@@ -34,7 +57,20 @@ public final class QuestionAnswering {
                         .optTranslatorFactory(new QuestionAnsweringTranslatorFactory())
                         .optProgress(new ProgressBar())
                         .build();
-                        
+
+        try (HuggingFaceTokenizer tokenizer = 
+                HuggingFaceTokenizer.builder()
+                        .optTokenizerName("bert-base-cased")
+                        .optAddSpecialTokens(false)
+                        .build()) {
+            for (int i = 0; i < testIds.length; ++i) {
+                String ret = tokenizer.decode(testIds[i]);
+                Assert.assertEquals(ret, expectedDecodedNoSpecialTokens[i]);
+                ret = tokenizer.decode(testIds[i], false);
+                Assert.assertEquals(ret, expectedDecodedWithSpecialTokens[i]);
+            }
+        }
+
         tokenizer = HuggingFaceTokenizer.newInstance(Paths.get(path));
         try (ZooModel<QAInput, String> model = criteria.loadModel();
                 Predictor<QAInput, String> predictor = model.newPredictor()) {
