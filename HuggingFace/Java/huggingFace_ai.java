265f5a94c9dc043194c1030f82c7d9c9ea4d2c00
Michal Feldman <michal@apiiro.com>
2023-10-29 12:47:46 +0200

    added examples for huggingface java

--- HuggingFace/Java/huggingFace_ai.java
+++ HuggingFace/Java/huggingFace_ai.java
@@ -17,8 +17,9 @@ public class Huggingfacesomething {
                 "BBC Japan was a general entertainment Channel. "
                         + "Which operated between December 2004 and April 2006. "
                         + "It ceased operations after its Japanese distributor folded.";
+        HuggingFaceTokenizer tokenizer = new HuggingFaceTokenizer();
         tokenizer = HuggingFaceTokenizer.newInstance(Paths.get(path));
-        HuggingFaceTokenizer tokenizer = new HuggingFaceTokenizer.newInstance(Paths.get(path));
+        HuggingFaceTokenizer tokenizer1 = new HuggingFaceTokenizer.newInstance(Paths.get(path));
         try (ZooModel<QAInput, String> model = criteria.loadModel();
                 Predictor<QAInput, String> predictor = model.newPredictor()) {
             QAInput input = new QAInput(question, paragraph);
